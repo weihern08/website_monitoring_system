b126f672352f4d67c06b8d4c501dcdfa4cbf32e8
NG WEI HERN <weihern08@gmail.com>
2026-08-20 10:38:06 +0800

    Delete website_monitoring_system directory

--- website_monitoring_system/admin/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once dirname(__DIR__) . '/includes/init.php';
-logoutAdmin();
-header('Location: ' . appUrl('login.php'));
-exit;
--- website_monitoring_system/includes/footer.php
+++
@@ -1,6 +0,0 @@
-        </div>
-    </div>
-</div>
-<script src="<?= e(appUrl('assets/js/app.js')) ?>"></script>
-</body>
-</html>
